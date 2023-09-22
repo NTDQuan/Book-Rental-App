fc9044501debf52503afa066b1d5d87fbe642e1c
Dinh Quan <ntdquan2003@gmail.com>
2023-09-22 08:45:17 +0700

    add splash screen

---
+++ app/src/main/java/com/kwan/bookrentalshopapp/Splash_Activity.java
@@ -0,0 +1,25 @@
+package com.kwan.bookrentalshopapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_activity);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(Splash_Activity.this, LoginActivity.class));
+                finish();
+            }
+        }, 2000);
+    }
+}
